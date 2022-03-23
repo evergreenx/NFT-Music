74ca6887a3e234a685fa3aebc25ec77ade14891a
Ido Evergreen <dicksonevergreenido@gmail.com>
2022-03-23 12:20:21 +0100

    fixing buid

--- .history/astro.config_20220322233502.mjs
+++ .history/astro.config_20220322233502.mjs
@@ -2,7 +2,7 @@ export default /** @type {import('astro').AstroUserConfig} */ ({
   // all options are optional; these values are the defaults
   projectRoot: "./",
   public: "./public/",
-  dist: "/dist",
+  dist: "./dist/",
   src: "./src/",
   pages: "./src/pages/",
   renderers: [
